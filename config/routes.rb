22e81d9090be7c01ad7cb73467fa2607ee20f43c
drumnation <davidmieloch@gmail.com>
2017-04-18 11:40:17 -0400

    built user controller, model, and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'users/set_user'
+
+  get 'users/user_params'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
