8c1e23d9296f87d18e53affce8baf564eaadb5de
Rob Weber <rob.weber87@gmail.com>
2017-04-23 13:22:22 -0400

    commiting to pull master

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,21 +3,4 @@ class Submission < ActiveRecord::Base
 	has_many :comments
 	has_many :votes
 
-	def total_upvotes
-    votes.where(upvote: true).count - votes.where(upvote: false).count
-  end
-
-  def update_score(gravity = 1.8)
-    votes = self.total_upvotes
-    age = Time.diff(Time.now, self.created_at)[:hour]
-    score = (votes + 1) / (age + 2) ** gravity * 1000
-    self.update_attribute(:score, score)
-  end
-
-  def Submission.update_scores
-    Submission.all.each do |submission|
-      submission.update_score()
-    end
-  end
-
 end
